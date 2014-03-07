07e140fd39992f1c55b27e2217d6b594fe5708f8
ontoquad <support.ontoquad@my-eventos.com>
2014-03-07 20:53:35 +0400

    ontoquad port changed

--- Contacts/name_phone_withdelete.py
+++ Contacts/name_phone_withdelete.py
@@ -24,7 +24,7 @@ def executeInsert(triples):
     urllib.urlopen(endpoint, params)
 
 def executeDelete(concept_uri):
-    endpoint = 'http://127.0.0.1:8080/sparql'
+    endpoint = 'http://127.0.0.1:8081/sparql'
 
     query = 'DELETE WHERE {\n'
     query += '?s ?p ?o.\n'
